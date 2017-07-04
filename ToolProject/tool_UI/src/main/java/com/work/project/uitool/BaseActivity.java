d99285636e89efe5f775fa10eadf534c91b8bfeb
bazengliang <bazengliang@oak-inc.com>
2017-07-04 16:46:25 +0800

    添加Activity基类

---
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/BaseActivity.java
@@ -0,0 +1,10 @@
+package com.work.project.uitool;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Acitivity基类
+ * Created by bazengliang on 2017/7/4.
+ */
+public class BaseActivity extends AppCompatActivity {
+}
