1339633a76fe91671d103d585ce17b84359bea92
runTop <bzlup.139.com>
2017-07-05 00:10:25 +0800

    添加测试数据

--- ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
@@ -6,12 +6,14 @@ import android.widget.TextView;
 import android.util.Log;
 
 public class MainActivity extends AppCompatActivity {
-
+    TextView textView2;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
         TextView textView;
+        Log.i("openBranch","cc");
         Log.i("aa","");
+        Log.i("openBranch","dd");
     }
 }
