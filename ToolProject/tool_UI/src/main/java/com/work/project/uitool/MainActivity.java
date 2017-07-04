6bd11b8f341f1e3b6f0af3d2756bb7cd0732d33d
bazengliang <bazengliang@oak-inc.com>
2017-07-04 16:53:26 +0800

    添加MainActivity注释

--- ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
@@ -3,6 +3,11 @@ package com.work.project.uitool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 定制ViewGroup
+ * @author bazengliang
+ * @date 2017-7-4
+ */
 public class MainActivity extends BaseActivity {
 
     @Override
