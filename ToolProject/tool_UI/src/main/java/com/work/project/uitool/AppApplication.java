379264a287d7249681102cd529fc95a04e1dd1d7
bazengliang <bazengliang@oak-inc.com>
2017-07-04 17:03:12 +0800

    更新AppApplication类

--- ToolProject/tool_UI/src/main/java/com/work/project/uitool/AppApplication.java
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/AppApplication.java
@@ -8,4 +8,14 @@ import android.app.Application;
 
 public class AppApplication extends Application {
     private static AppApplication instance;
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+        instance = this;
+    }
+
+    public static AppApplication getInstance() {
+        return instance;
+    }
 }
