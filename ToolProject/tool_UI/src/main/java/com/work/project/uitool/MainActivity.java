8868d0487c9d569978be2576b78328d6597af937
runTop <bzlup.139.com>
2017-07-05 00:09:01 +0800

    测试分支dev2提交

--- ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
@@ -11,7 +11,9 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.i("dev2","aaa");
         TextView textView;
         Log.i("aa","");
+        Log.i("dev2","bbb");
     }
 }
