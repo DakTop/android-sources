daa4931e558aa3a4a55970548cbffcbb23909b12
runTop <bzlup.139.com>
2017-08-09 00:31:59 +0800

    add notes

--- ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
+++ ToolProject/tool_UI/src/main/java/com/work/project/uitool/MainActivity.java
@@ -3,6 +3,9 @@ package com.work.project.uitool;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
+/**
+ * 打造一系列自定义View
+ */
 public class MainActivity extends AppCompatActivity {
 
     @Override
